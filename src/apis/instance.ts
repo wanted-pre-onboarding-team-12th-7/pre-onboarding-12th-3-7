9a04edd68ba5804e0e9c07ca5dda363d857a5456
Seungrok <sonicrok93@gmail.com>
2023-09-06 20:54:28 +0900

    feat: axiosInstance 초기화 & suggestion get API 함수 추가

---
+++ src/apis/instance.ts
@@ -0,0 +1,37 @@
+import axios from 'axios'
+
+const BASE_URL = 'https://json-server-6gjfchfpb-wanted-team7.vercel.app/'
+
+const axiosInstance = axios.create({ baseURL: BASE_URL })
+
+axiosInstance.interceptors.request.use((config) => {
+  /** @description API 호출될 때마다 console.info 출력*/
+  console.info('API CALL to ', config.url, config.params)
+  return config
+})
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    /** @description 200대 응답이라면 이 콜백이 실행됨*/
+    return response
+  },
+  (error) => {
+    /** @description 200대 응답이 아니라면 이 콜백이 실행됨*/
+    console.error(error)
+    return Promise.reject(error)
+  }
+)
+
+export interface QueryParam {
+  [key: string]: string
+  q: string
+}
+
+const instance = {
+  get: <T>(url: string, param: QueryParam) =>
+    axiosInstance.get<T>(url, {
+      params: param,
+    }),
+}
+
+export { instance }
