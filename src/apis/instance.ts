4b80ce8085826ab6180429e7779e6602e1113d46
Seungrok <sonicrok93@gmail.com>
2023-09-06 23:03:10 +0900

    chore: 불필요한 주석 삭제

--- src/apis/instance.ts
+++ src/apis/instance.ts
@@ -5,18 +5,13 @@ const BASE_URL = 'https://json-server-6gjfchfpb-wanted-team7.vercel.app/'
 const axiosInstance = axios.create({ baseURL: BASE_URL })
 
 axiosInstance.interceptors.request.use((config) => {
-  /** @description API 호출될 때마다 console.info 출력*/
   console.info('calling api')
   return config
 })
 
 axiosInstance.interceptors.response.use(
-  (response) => {
-    /** @description 200대 응답이라면 이 콜백이 실행됨*/
-    return response
-  },
+  (response) => response,
   (error) => {
-    /** @description 200대 응답이 아니라면 이 콜백이 실행됨*/
     console.error(error)
     return Promise.reject(error)
   }
